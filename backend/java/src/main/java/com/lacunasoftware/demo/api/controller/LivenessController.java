0de0fd38e6fed5dc571ec163a52259ce927888ad
Gabriel Sadrack <gabrielc@lacunasoftware.com>
2025-12-05 08:55:01 -0300

    Added examples for status and ticket endpoints

--- backend/java/src/main/java/com/lacunasoftware/demo/api/controller/LivenessController.java
+++ backend/java/src/main/java/com/lacunasoftware/demo/api/controller/LivenessController.java
@@ -1,7 +1,10 @@
 package com.lacunasoftware.demo.api.controller;
 
+import java.util.UUID;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.lacunasoftware.restpkicore.*;
@@ -10,25 +13,37 @@ import com.lacunasoftware.restpkicore.*;
 @RequestMapping("api/bio/liveness")
 public class LivenessController {
 
-    @GetMapping()
-    public ResponseEntity<String> livenessExample() throws Exception {
+    private RestBioService getService() {
         RestPkiOptions restPkiOptions = new RestPkiOptions();
-
         restPkiOptions.setEndpoint("https://example.core.pki.rest/");
         restPkiOptions.setApiKey("apykey|xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+        return RestBioServiceFactory.getService(restPkiOptions);
+    }
 
-        RestBioService service = RestBioServiceFactory.getService(restPkiOptions);
+    @GetMapping()
+    public ResponseEntity<StartBioSessionResponse> livenessExample() throws Exception {
+        RestBioService service = getService();
 
         StartLivenessSessionRequest request = new StartLivenessSessionRequest();
-
         request.setSubjectIdentifier("f6bf3b8f-726f-4dad-b544-173862dc1223");
         request.setTrustedOrigin("http://localhost:8080/");
 
         StartBioSessionResponse bioSessionResponse = service.StartLivenessSessionAsync(request);
+        return ResponseEntity.ok(bioSessionResponse);
+    }
 
-        return ResponseEntity.ok()
-                .header("Content-Type", "text/html; charset=utf-8")
-                .body(bioSessionResponse.getSessionUrl());
+    @GetMapping("/status/{sessionId}")
+    public ResponseEntity<FaceLivenessSessionStatusInfo> livenessSessionStatus(@PathVariable UUID sessionId) throws Exception {
+        RestBioService service = getService();
+        LivenessSessionStatusModel status = service.GetLivenessSessionStatusAsync(sessionId);
+        return ResponseEntity.ok(status.getFaceLivenessStatus());
+    }
+
+    @GetMapping("/complete/{ticket}")
+    public ResponseEntity<String> completeLivenessSession(@PathVariable String ticket) throws Exception {
+        RestBioService service = getService();
+        LivenessSessionStatusModel result = service.CompleteLivenessSessionAsync(ticket);
+        return ResponseEntity.ok(result.toString());
     }
 
 }
